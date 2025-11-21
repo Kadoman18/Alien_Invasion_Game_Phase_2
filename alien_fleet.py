c8ec7784c039ccde9f1098633b4e73a59d5bb696
Shrrayashwinz <sshrrayash@gmail.com>
2025-11-21 10:53:17 -0500

    Enjoy!

--- alien_fleet.py
+++ alien_fleet.py
@@ -3,7 +3,7 @@ Program Name: alien_fleet.py
 
 Author: Shrrayash Srinivasan
 
-Purpose: Handles the alien fleet movement and whatnot. enjoy!
+Purpose: Handles the alien fleet movement and whatnot. Enjoy!
 
 Date: November 21, 2025
 """
